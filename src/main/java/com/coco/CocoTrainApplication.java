d7d4bafe892797ce26e56e21974cd66c06a1b8f0
GiMin <gitgtgtg@naver.com>
2023-12-18 17:51:08 +0900

    API테스트 완료

--- src/main/java/com/coco/CocoTrainApplication.java
+++ src/main/java/com/coco/CocoTrainApplication.java
@@ -2,10 +2,12 @@ package com.coco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.popo.service"})
+@ComponentScan(basePackages = {"com.coco.service"})
+//@EntityScan(basePackages = {"com.coco.domain"})
 public class CocoTrainApplication {
 
 	public static void main(String[] args) {
